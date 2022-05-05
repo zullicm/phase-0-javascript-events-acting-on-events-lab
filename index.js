258e7b84ab03e2b3f24568fcdcd716d416e57a0b
Christopher Marzulli <chriszulli15@gmail.com>
2022-05-05 13:40:57 -0400

    commit tree 7f9e1ad260830593c017b0bc5b5c36a7ab5945b0

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e){
+  if (e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerRight(){
+   const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
